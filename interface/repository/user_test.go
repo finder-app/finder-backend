68a12a316ced3350710cbd56d9bcddf2bb17226c
kaito_ohishi <kaito990626@yahoo.o.jp+ohishikaito@users.noreply.github.com>
2021-05-11 10:00:14 +0900

    TestGetUserByUidテストを作成

--- interface/repository/user_test.go
+++ interface/repository/user_test.go
@@ -26,12 +26,7 @@ func NewGormConnectMock() (*gorm.DB, sqlmock.Sqlmock, error) {
 	return db, mock, nil
 }
 
-func TestGetUsersByGender(t *testing.T) {
-	db, mock, err := NewGormConnectMock()
-	if err != nil {
-		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
-	}
-
+func setMockUsers() []domain.User {
 	mockUsers := []domain.User{
 		{
 			Uid:       "Uid",
@@ -41,7 +36,6 @@ func TestGetUsersByGender(t *testing.T) {
 			IsMale:    true,
 			CreatedAt: time.Now(),
 			UpdatedAt: time.Now(),
-			DeletedAt: nil,
 		},
 		{
 			Uid:       "Uid2",
@@ -51,9 +45,17 @@ func TestGetUsersByGender(t *testing.T) {
 			IsMale:    false,
 			CreatedAt: time.Now(),
 			UpdatedAt: time.Now(),
-			DeletedAt: nil,
 		},
 	}
+	return mockUsers
+}
+
+func TestGetUsersByGender(t *testing.T) {
+	db, mock, err := NewGormConnectMock()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	mockUsers := setMockUsers()
 
 	maleRows := sqlmock.NewRows([]string{
 		"uid",
@@ -74,7 +76,6 @@ func TestGetUsersByGender(t *testing.T) {
 		mockUsers[0].UpdatedAt,
 		mockUsers[0].DeletedAt,
 	)
-
 	femaleRows := sqlmock.NewRows([]string{
 		"uid",
 		"email",
@@ -83,7 +84,6 @@ func TestGetUsersByGender(t *testing.T) {
 		"is_male",
 		"created_at",
 		"updated_at",
-		"deleted_at",
 	}).AddRow(
 		mockUsers[1].Uid,
 		mockUsers[1].Email,
@@ -92,7 +92,6 @@ func TestGetUsersByGender(t *testing.T) {
 		mockUsers[1].IsMale,
 		mockUsers[1].CreatedAt,
 		mockUsers[1].UpdatedAt,
-		mockUsers[1].DeletedAt,
 	)
 
 	male := true
@@ -111,4 +110,39 @@ func TestGetUsersByGender(t *testing.T) {
 	femaleUsers, err := userRepository.GetUsersByGender(female)
 	assert.NoError(t, err)
 	assert.Len(t, femaleUsers, 1)
+}
+
+func TestGetUserByUid(t *testing.T) {
+	db, mock, err := NewGormConnectMock()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	mockUsers := setMockUsers()
+	rows := sqlmock.NewRows([]string{
+		"uid",
+		"email",
+		"last_name",
+		"first_name",
+		"is_male",
+		"created_at",
+		"updated_at",
+	}).AddRow(
+		mockUsers[0].Uid,
+		mockUsers[0].Email,
+		mockUsers[0].LastName,
+		mockUsers[0].FirstName,
+		mockUsers[0].IsMale,
+		mockUsers[0].CreatedAt,
+		mockUsers[0].UpdatedAt,
+	)
+	query := regexp.QuoteMeta("SELECT * FROM `users` WHERE `users`.`deleted_at` IS NULL AND ((uid = ?)) LIMIT 1")
+	uid := mockUsers[0].Uid
+	mock.ExpectQuery(query).WithArgs(uid).WillReturnRows(rows)
+
+	validate := validator.New()
+	userRepository := repository.NewUserRepository(db, validate)
+	user, err := userRepository.GetUserByUid(uid)
+	assert.NoError(t, err)
+	assert.NotNil(t, user)
+	assert.Equal(t, user.Uid, mockUsers[0].Uid)
 }
